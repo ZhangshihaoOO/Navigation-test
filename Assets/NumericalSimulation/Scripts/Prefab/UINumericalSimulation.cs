66e92be477978fd1dde460cc22d5d5f17034448d
liuYongChao <1215266098@qq.com>
2024-08-03 10:58:49 +0800

    添加excel->json工具

---
+++ Assets/NumericalSimulation/Scripts/Prefab/UINumericalSimulation.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+namespace NumericalSimulation.Scripts.Prefab
+{
+    public partial class UINumericalSimulation : MonoBehaviour
+    {
+        public TextAsset data;
+    }
+}
